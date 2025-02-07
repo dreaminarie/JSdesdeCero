94d5c0a2078e3f3ca7f572bcde8f56668709d46c
Jaz González <isntyoongi@gmail.com>
2025-02-06 18:40:55 -0600

    Agregué un archivo html para poder manipular la consola en el navegador y añadí la información de la primera práctica

---
+++ tipos-de-dato.js
@@ -0,0 +1,14 @@
+console.log(typeof 42); //number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean
+console.log(typeof 3.14); // number
+console.log(typeof Math); // object
+console.log(typeof alert); // function
+console.log(typeof 'easteregg'); // string
+console.log(typeof NaN); // number (Not a Number)
